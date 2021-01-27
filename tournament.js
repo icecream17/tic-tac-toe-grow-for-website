88d74b571e12aa5e6a7cecaecff66dfde6292395
steven nguyen <58114641+icecream17@users.noreply.github.com>
2021-01-27 10:48:21 -0600

    non comment

--- tournament.js
+++ tournament.js
@@ -1,46 +1,71 @@
 // TODO
-// class Tournament {}
-
-/**
 let bot_iterator = Object.entries(bot_mechanics);
-let currentBots = [0, 0];
-let repeat = 100;
-for (let bot of bot_iterator) {
-    bot[1].games = []
-}
-function doGame () {
-    if (currentBots[0] === bot_iterator.length) {
-        currentBots = [0, 0]; repeat--;
-        if (repeat === 0) clearInterval(tournamentInterval);
-        return;
-    }
-    if (!currentGame.result && currentBots[0] + currentBots[1] !== 0) return;
-    ELEMENTS.getPlayerSelects()[0].selectedIndex = 0;
-    ELEMENTS.getPlayerSelects()[0].dispatchEvent(new Event("change"));
-    if (currentBots[0] + currentBots[1] !== 0) ELEMENTS.resetGameButton.click();
-    ELEMENTS.getPlayerSelects()[0].selectedIndex = 4 + currentBots[0];
-    ELEMENTS.getPlayerSelects()[1].selectedIndex = 4 + currentBots[1];
-    ELEMENTS.getPlayerSelects()[1].dispatchEvent(new Event("change"));
-    ELEMENTS.getPlayerSelects()[0].dispatchEvent(new Event("change"));
-    bot_iterator[currentBots[0]][1].games.push(currentGame);
-    bot_iterator[currentBots[1]][1].games.push(currentGame);
-    currentBots[1]++;
-    if (currentBots[1] === bot_iterator.length) {
-        currentBots[1] = 0;
-        currentBots[0]++;
-        if (currentBots[0] === bot_iterator.length) return true;
-    }
-    return false;
-}
-Game.prototype.doBotMove = function () {
-  if (players[this.toMove].player.type === "bot")
-     // if (this.result and tournament mode
-     if (this.result) console.info("Game ended");
-     else players[this.toMove].player.play();
-  else
-     console.info("Player must've changed into a human");
-}
-let tournamentInterval = setInterval(doGame, 4000);
+const NON_TOURNAMENT_BOT_MOVE_FUNC = Game.prototype.doBotMove;
+const PlayerFields = Array.from(document.querySelectorAll("#choosePlayerFields select"))
+let tournaments = [];
 
+class Tournament {
+   constructor (rounds, interval=4000) {
+      this.currentBots = [0, 0];
+      this.rounds = rounds;
+      this.currentRound = 0;
+      this.interval = interval;
+      this.intervalID = null;
+   }
 
-*/
+   start () {
+      Game.prototype.doBotMove = Tournament.botMoveFunc;
+      this.intervalID = setInterval(doGame, this.interval);
+   }
+
+   finish () {
+      clearInterval(this.intervalID);
+      this.intervalID = null;
+      Game.prototype.doBotMove = NON_TOURNAMENT_BOT_MOVE_FUNC;
+      tournaments.push(this);
+   }
+
+   playGame () {
+      if (this.currentBots[0] === bot_iterator.length) {
+         this.currentBots = [0, 0];
+         this.currentRound++;
+
+         if (this.currentRound === this.rounds)
+            this.finish();
+
+         return;
+      }
+      if (!this.currentGame.result &&
+         this.currentBots[0] + this.currentBots[1] !== 0) return;
+        
+      PlayerFields[0].selectedIndex = 0;
+      PlayerFields[0].dispatchEvent(new Event("change"));
+
+      if (this.currentBots[0] + this.currentBots[1] !== 0)
+         ELEMENTS.resetGameButton.click();
+
+      PlayerFields[0].selectedIndex = 4 + this.currentBots[0];
+      PlayerFields[1].selectedIndex = 4 + this.currentBots[1];
+      PlayerFields[1].dispatchEvent(new Event("change"));
+      PlayerFields[0].dispatchEvent(new Event("change"));
+      this.games.push([currentGame, this.currentBots[0], this.currentBots[1]]);
+
+      this.currentBots[1]++;
+      if (this.currentBots[1] === bot_iterator.length) {
+         this.currentBots[1] = 0;
+         this.currentBots[0]++;
+         if (this.currentBots[0] === bot_iterator.length)
+            return true;
+      }
+      return false;
+   }
+    
+   static botMoveFunc () {
+      if (players[this.toMove].player.type === "bot")
+         // if (this.result and tournament mode
+         if (this.result) console.info("Game ended");
+         else players[this.toMove].player.play();
+      else
+         console.info("Player must've changed into a human");
+   }
+}
