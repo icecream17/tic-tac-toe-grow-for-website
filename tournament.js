dd1e881c54de9518e8ad631c180f25c6ecdf8d04
steven nguyen <58114641+icecream17@users.noreply.github.com>
2021-01-27 09:46:59 -0600

    fix bugs [skip ci]

--- tournament.js
+++ tournament.js
@@ -8,16 +8,18 @@ for (let bot of bot_iterator) {
     bot[1].games = []
 }
 function doGame () {
-    if (currentBots[0] === bot_iterator.length) return;
-
+    if (currentBots[0] === bot_iterator.length) {
+        clearInterval(tournamentInterval);
+        return;
+    }
+    if (!currentGame.result && currentBots[0] + currentBots[1] !== 0) return;
     ELEMENTS.getPlayerSelects()[0].selectedIndex = 0;
     ELEMENTS.getPlayerSelects()[0].dispatchEvent(new Event("change"));
-
+    if (currentBots[0] + currentBots[1] !== 0) ELEMENTS.resetGameButton.click();
     ELEMENTS.getPlayerSelects()[0].selectedIndex = 4 + currentBots[0];
     ELEMENTS.getPlayerSelects()[1].selectedIndex = 4 + currentBots[1];
     ELEMENTS.getPlayerSelects()[1].dispatchEvent(new Event("change"));
     ELEMENTS.getPlayerSelects()[0].dispatchEvent(new Event("change"));
-    ELEMENTS.resetGameButton.click();
     bot_iterator[currentBots[0]][1].games.push(currentGame);
     bot_iterator[currentBots[1]][1].games.push(currentGame);
     currentBots[1]++;
@@ -31,10 +33,12 @@ function doGame () {
 Game.prototype.doBotMove = function () {
   if (players[this.toMove].player.type === "bot")
      // if (this.result and tournament mode
-     if (this.result) {console.info("Game ended"); doGame();}
+     if (this.result) console.info("Game ended");
      else players[this.toMove].player.play();
   else
      console.info("Player must've changed into a human");
 }
+let tournamentInterval = setInterval(doGame, 4000);
+
 
 */
