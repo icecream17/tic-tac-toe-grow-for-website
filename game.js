4789f0420aa1283a5113a98d62430b50c9c6f3ce
steven nguyen <58114641+icecream17@users.noreply.github.com>
2021-02-04 09:55:18 -0600

    non-game limits

--- game.js
+++ game.js
@@ -322,11 +322,11 @@ class GameBase {
          return ["draw", "width is 7 times the height"];
       else if (this.board.height > 7 * this.board.width)
          return ["draw", "height is 7 times the width"];
-      else if (this.turn >= Game.MAX_TURNS)
+      else if (this.turn >= this.MAX_TURNS)
          return ["draw", `max turns reached (${Game.MAX_TURNS})`]
-      else if (this.board.width >= Game.MAX_LENGTH)
+      else if (this.board.width >= this.MAX_LENGTH)
          return ["draw", `max length reached by width (${Game.MAX_LENGTH})`]
-      else if (this.board.height >= Game.MAX_LENGTH)
+      else if (this.board.height >= this.MAX_LENGTH)
          return ["draw", `max length reached by height (${Game.MAX_LENGTH})`]
       else
          return false;
