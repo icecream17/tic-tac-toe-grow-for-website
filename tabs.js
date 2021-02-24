0daa2034b314ce6c3291708c456feeb325408a8d
steven nguyen <icecream17.github@gmail.com>
2021-02-24 10:52:39 -0600

    Create tabs.js

---
+++ tabs.js
@@ -0,0 +1,15 @@
+// global ELEMENTS
+
+ELEMENTS.TabElements = [
+   document.getElementById("tabInfo"),
+   document.getElementById("gameControls"),
+   document.getElementById("gameData"),
+   document.getElementById("playerSettings"),
+];
+
+ELEMENTS.CurrentTab = ELEMENTS.TabElements[0];
+
+document.getElementById("tabSelect").onchange = function changeTab () {
+   ELEMENTS.CurrentTab.classList.remove("selectedTab");
+   ELEMENTS.TabElements[this.selectedIndex].classList.add("selectedTab");
+}
