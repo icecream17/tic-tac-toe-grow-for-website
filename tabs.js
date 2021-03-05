5f8144beab14e5584e667e072d158a493fbfb3e6
steven nguyen <icecream17.github@gmail.com>
2021-03-05 15:32:28 -0600

    fix shift+left arrow key & unintuitive up/down

--- tabs.js
+++ tabs.js
@@ -17,20 +17,18 @@ ELEMENTS.TabSelect.onchange = function changeTab () {
    ELEMENTS.CurrentTab = ELEMENTS.TabElements[this.selectedIndex];
 }
 
-const KeyCodes = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"];
-const ForwardKeyCodes = [KeyCodes[0], KeyCodes[3]];
-const BackwardKeyCodes = [KeyCodes[1], KeyCodes[2]];
-
 document.addEventListener(
    "keydown",
    function TabChange (event) {
       if (event.shiftKey) {
-         if (ForwardKeyCodes.includes(event.key)) {
+         // In a dropdown, going down the list === going forward
+         if (event.key === "ArrowDown" || event.key === "ArrowRight") {
             // I wish I could use the with statement
             ELEMENTS.TabSelect.selectedIndex = (ELEMENTS.TabSelect.selectedIndex + 1) % ELEMENTS.TabSelect.options.length;
             ELEMENTS.TabSelect.dispatchEvent(new Event("change"));
-         } else if (BackwardKeyCodes.includes(event.key)) {
-            ELEMENTS.TabSelect.selectedIndex = (ELEMENTS.TabSelect.selectedIndex - 1) % ELEMENTS.TabSelect.options.length;
+         } else if (event.key === "ArrowUp" || event.key === "ArrowLeft") {
+            // Add options.length so that pressing left won't get a -1 index
+            ELEMENTS.TabSelect.selectedIndex = (ELEMENTS.TabSelect.selectedIndex + ELEMENTS.TabSelect.options.length - 1) % ELEMENTS.TabSelect.options.length;
             ELEMENTS.TabSelect.dispatchEvent(new Event("change"));
          }
       }
