1302c058b6df763b0ef88cea02530e114be00546
steven nguyen <icecream17.github@gmail.com>
2021-04-20 07:46:38 -0500

    Update and rename everything.test.js to everything.test.ts

--- test/everything.test.js
+++
@@ -1,8 +0,0 @@
-import * as utils from '../js/utils.js'
-import * as errors from '../js/errors.js'
-import * as game from '../js/game.js'
-import * as tabs from '../js/tabs.js'
-import * as tournament from '../js/tournament.js'
-import * as debug from '../js/debug.js'
-
-test.todo('add tests')
