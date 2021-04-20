cae1520b0e2de222ffc3da39226b25eed3cc5b7b
steven nguyen <icecream17.github@gmail.com>
2021-04-20 10:55:46 -0500

    fix comments

--- test/everything.test.js
+++ test/everything.test.js
@@ -1,7 +1,7 @@
-import * as utils from '../js/utils.js' // eslint-ignore-line no-unused-vars
-import * as errors from '../js/errors.js' // eslint-ignore-line no-unused-vars
-import * as game from '../js/game.js' // eslint-ignore-line no-unused-vars
-import * as tournament from '../js/tournament.js' // eslint-ignore-line no-unused-vars
-import * as debug from '../js/debug.js' // eslint-ignore-line no-unused-vars
+import * as utils from '../js/utils.js' // eslint-disable-line no-unused-vars
+import * as errors from '../js/errors.js' // eslint-disable-line no-unused-vars
+import * as game from '../js/game.js' // eslint-disable-line no-unused-vars
+import * as tournament from '../js/tournament.js' // eslint-disable-line no-unused-vars
+import * as debug from '../js/debug.js' // eslint-disable-line no-unused-vars
 
 test.todo('add tests')
