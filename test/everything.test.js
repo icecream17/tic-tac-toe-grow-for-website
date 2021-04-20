cb18a48fe7fae0858b5201854b478d730a826b2f
steven nguyen <icecream17.github@gmail.com>
2021-04-20 10:52:23 -0500

    tabs is not a module

--- test/everything.test.js
+++ test/everything.test.js
@@ -1,7 +1,6 @@
 import * as utils from '../js/utils.js' // eslint-ignore-line no-unused-vars
 import * as errors from '../js/errors.js' // eslint-ignore-line no-unused-vars
 import * as game from '../js/game.js' // eslint-ignore-line no-unused-vars
-import * as tabs from '../js/tabs.js' // eslint-ignore-line no-unused-vars
 import * as tournament from '../js/tournament.js' // eslint-ignore-line no-unused-vars
 import * as debug from '../js/debug.js' // eslint-ignore-line no-unused-vars
 
