ef926ffaa71649c5fd3eb98ae2bce99a284806ff
steven nguyen <icecream17.github@gmail.com>
2021-04-23 12:44:36 -0500

    add tests, unasync code, simplify

--- test/everything.test.js
+++ test/everything.test.js
@@ -1,32 +1,55 @@
-const imports = {}
+const fs = require('fs')
+const path = require('path')
 
-;(function setupHtml () {
-   const fs = require('fs')
-   const path = require('path')
+// The current directory is assumed to be either of:
+// <root repo dir>
+// <root repo dir>/tests
+const currentDir = path.resolve()
+const repoRootDir = 
+   currentDir.includes('tests')
+      ? path.join(currentDir, '..')
+      : currentDir
 
-   // The current directory is assumed to be either of:
-   // <root repo dir>
-   // <root repo dir>/tests
-   const currentDir = path.resolve()
-   const htmlPath =
-      currentDir.includes('tests')
-         ? path.join(currentDir, '..', 'game.html')
-         : path.join(currentDir, 'game.html')
-
+// Setup html
+{
+   const htmlPath = path.join(repoRootDir, 'game.html')
    document.write(fs.readFileSync(htmlPath))
    document.close() // "Tells the browser to finish loading the page"
    // https://developer.mozilla.org/en-US/docs/Web/API/Document/write
-})()
-
-;(async function importEverything () {
-   imports.utils = await import('../js/utils.js')
-   imports.errors = await import('../js/errors.js')
-   imports.game = await import('../js/game.js')
-   imports.tournament = await import('../js/tournament.js')
-   imports.debug = await import('../js/debug.js')
-   main()
-})()
-
-function main () {
-   test.todo('add tests')
 }
+
+
+// Setup imports
+const imports = {}
+{
+   const jsDirPath = path.join(repoRootDir, 'js')
+   function importModule (modulePath) {
+      return require(path.join(jsDirPath, modulePath))
+   }
+
+   imports.utils = importModule('../js/utils.js')
+   imports.errors = importModule('../js/errors.js')
+   imports.game = importModule('../js/game.js')
+   imports.tournament = importModule('../js/tournament.js')
+   imports.debug = importModule('../js/debug.js')
+}
+
+
+describe('check that setup worked', () => {
+   test('html', () => {
+      expect(document.body.innerText.includes('Tic tac toe grow')).toBe(true)
+   })
+
+   test('imports', () => {
+      const importKeys = ['utils', 'errors', 'game', 'tournmanet', 'debug']
+      expect(Objects.keys(imports)).toBeEqual(importKeys)
+      for (const key of importKeys) {
+         expect(imports[key]).toBeInstanceOf(Object)
+      }
+   })
+})
+
+
+// Main code
+test.todo('add tests')
+
