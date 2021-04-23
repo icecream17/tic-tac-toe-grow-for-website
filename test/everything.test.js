a5c94d73797bfbec2fb8c94cb9c9427cbc5bed16
steven nguyen <icecream17.github@gmail.com>
2021-04-23 11:02:52 -0500

    update html

--- test/everything.test.js
+++ test/everything.test.js
@@ -1,7 +1,23 @@
-import * as utils from '../js/utils.js' // eslint-disable-line no-unused-vars
-import * as errors from '../js/errors.js' // eslint-disable-line no-unused-vars
-import * as game from '../js/game.js' // eslint-disable-line no-unused-vars
-import * as tournament from '../js/tournament.js' // eslint-disable-line no-unused-vars
-import * as debug from '../js/debug.js' // eslint-disable-line no-unused-vars
+let imports = {}
 
-test.todo('add tests')
+{
+   (function setupHtml() {
+       const fs = require('fs')
+       document.write(fs.readFileSync('../game.html'))
+       document.close() // "Tells the browser to finish loading the page"
+       // https://developer.mozilla.org/en-US/docs/Web/API/Document/write
+   })()
+
+   (async function () {
+      imports.utils = await import('../js/utils.js')
+      imports.errors = await import('../js/errors.js')
+      imports.game = await import('../js/game.js')
+      imports.tournament = await import('../js/tournament.js')
+      imports.debug = await import('../js/debug.js')
+      main()
+   })()
+}
+
+function main () {
+   test.todo('add tests')
+}
