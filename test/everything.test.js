6e5cd387882097fd4d1b0f3d6769314c995853ac
steven nguyen <icecream17.github@gmail.com>
2021-04-23 11:09:34 -0500

    standardify

--- test/everything.test.js
+++ test/everything.test.js
@@ -1,22 +1,20 @@
-let imports = {}
+const imports = {}
 
-{
-   (function setupHtml() {
-       const fs = require('fs')
-       document.write(fs.readFileSync('../game.html'))
-       document.close() // "Tells the browser to finish loading the page"
-       // https://developer.mozilla.org/en-US/docs/Web/API/Document/write
-   })()
+(function setupHtml () {
+   const fs = require('fs')
+   document.write(fs.readFileSync('../game.html'))
+   document.close() // "Tells the browser to finish loading the page"
+   // https://developer.mozilla.org/en-US/docs/Web/API/Document/write
+})()
 
-   (async function () {
-      imports.utils = await import('../js/utils.js')
-      imports.errors = await import('../js/errors.js')
-      imports.game = await import('../js/game.js')
-      imports.tournament = await import('../js/tournament.js')
-      imports.debug = await import('../js/debug.js')
-      main()
-   })()
-}
+;(async function importEverything () {
+   imports.utils = await import('../js/utils.js')
+   imports.errors = await import('../js/errors.js')
+   imports.game = await import('../js/game.js')
+   imports.tournament = await import('../js/tournament.js')
+   imports.debug = await import('../js/debug.js')
+   main()
+})()
 
 function main () {
    test.todo('add tests')
