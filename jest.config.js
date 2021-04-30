bf64ea0dda7fdb19d8ced60e3ce3d6318fcd6ff5
steven nguyen <icecream17.github@gmail.com>
2021-04-30 14:02:13 -0500

    linting [skip ci]

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 const { DEFAULT_JEST_CONFIG } = require('jest-config')
 
-let newConfig = Object.assign({}, DEFAULT_JEST_CONFIG)
+const newConfig = Object.assign({}, DEFAULT_JEST_CONFIG)
 
 module.exports = Object.assign(newConfig, {
    setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
