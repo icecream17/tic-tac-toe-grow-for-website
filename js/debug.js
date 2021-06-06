af21bb448b84ab119b11064177cf5632cf26c392
steven nguyen <nguyeste008@students.garlandisd.net>
2021-06-06 14:17:46 -0500

    export `imports`

--- js/debug.js
+++ js/debug.js
@@ -1,14 +1,13 @@
-import {} from './errors.js'
-import {} from './game.js'
-import {} from './players.js'
-import {} from './tabs.js'
-import { Tournament } from './tournament.js'
-import {} from './utils.js'
+import * as errors from './errors.js'
+import * as game from './game.js'
+import * as players from './players.js'
+import * as tabs from './tabs.js'
+import * as tournament from './tournament.js'
+import * as utils from './utils.js'
 
-export const moduleGlobals = {
-   Tournament,
-   bots
+export const imports = {
+   errors, game, players, tabs, tournament, utils
 }
 
 // So that module globals can be used in the console
-Object.assign(globalThis, moduleGlobals)
+Object.assign(globalThis, imports)
