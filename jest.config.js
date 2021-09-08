29e1f62c1d17db82186297a7c33e88dfa272e857
steven nguyen <nguyeste008@students.garlandisd.net>
2021-09-08 18:22:30 -0500

    add test environment

--- jest.config.js
+++ jest.config.js
@@ -4,5 +4,6 @@ const newConfig = Object.assign({}, DEFAULT_JEST_CONFIG)
 
 // Use 2 spaces for import in this file to avoid creating yet another eslintrc
 module.exports = Object.assign(newConfig, {
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js']
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+  testEnvironment: 'jsdom'
 })
